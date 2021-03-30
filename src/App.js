a3e301dd99d2c674e095e8d4806e8b2ae340d489
Jens Kristoffersen <jensakrr@gmail.com>
2021-03-30 15:31:45 +0200

    Add simple fetch to API via cors-anywhere.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import axios from "axios";
 import Main from "./Main/Main";
+
+axios.defaults.baseURL =
+  "https://vozwntwomh.execute-api.us-east-2.amazonaws.com/Production/";
 
 function App() {
   return (
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,16 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 
 function Main() {
   const [searchText, setSearchText] = useState("");
-  const onSearchTextChange = (e) => {
-    setSearchText(e.target.value);
-  };
+
+  useEffect(() => {
+    if (searchText !== "") {
+      fetch(
+        "https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=" +
+          searchText
+      )
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => {
+          return error;
+        });
+    }
+  }, [searchText]);
+
   return (
     <div>
       <SearchBar
         searchText={searchText}
-        onSearchTextChange={onSearchTextChange}
+        onSearchTextChange={(e) => setSearchText(e.target.value)}
       />
     </div>
   );
